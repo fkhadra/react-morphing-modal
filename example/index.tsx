9b6199267b71c8d0127c0b849c89890faccd8f15
Fadi Khadra <fdkhadra@gmail.com>
2019-07-01 14:33:00 +0200

    add working example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,30 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { useModal, Modal } from '../.';
+
+import '../dist/ReactFancyModal.css';
+import './index.css';
 
 const App = () => {
+  const { modalProps, open, triggerRef } = useModal();
   return (
     <div>
-      <Thing />
+      <div className="card">
+        <img src="https://picsum.photos/300" alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>John Doe</b>
+          </h4>
+          <div className="action">
+            <span>Engineer</span>
+            <button ref={triggerRef as any} onClick={open}>
+              Read More
+            </button>
+          </div>
+        </div>
+      </div>
+      <Modal {...modalProps}>hello good morning</Modal>
     </div>
   );
 };
