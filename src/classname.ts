9460584a9250fc62cab4d56249af28a92321a21d
Fadi Khadra <fdkhadra@gmail.com>
2019-07-01 13:46:45 +0200

    fix className namespace

--- src/classname.ts
+++ src/classname.ts
@@ -1,8 +1,8 @@
 export const classname = {
-  container: 'Fancy__container',
-  closeButton: 'Fancy__close-button',
-  placeholder: 'Fancy__placeholder',
-  body: 'Fancy__body',
+  container: 'RFM__container',
+  closeButton: 'RFM__close-button',
+  placeholder: 'RFM__placeholder',
+  body: 'RFM__body',
   get(className: string, isActive: boolean) {
     return isActive ? `${className} ${className}--is-active` : className;
   },
